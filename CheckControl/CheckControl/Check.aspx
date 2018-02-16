ebf2208697304a0ba19b543fd446cd45f2a9fe06
abc <you@example.com>
2018-02-16 20:06:04 +0600

    adding checkbox

---
+++ CheckControl/CheckControl/Check.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Check.aspx.cs" Inherits="CheckControl.Check" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" OnSelectedIndexChanged="CheckBoxList1_SelectedIndexChanged" RepeatDirection="Horizontal">
+
+                        <asp:ListItem Text="Diploma"  Value="1" ></asp:ListItem>
+                        <asp:ListItem Text="Graduate"   Value="2" ></asp:ListItem>
+                        <asp:ListItem Text="Non-Gradutae"  Value="3" ></asp:ListItem>
+
+        </asp:CheckBoxList>
+    <div>
+    
+    
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+    
+    
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Seclect All" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Deselctall" />
+    
+    
+    </form>
+</body>
+</html>
---
+++ CheckControl/CheckControl/Check.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CheckControl
+{
+    public partial class Check : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CheckBoxList1.SelectedIndex = 1;
+            }
+        }
+
+        protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+
+                if (li.Selected)
+                {
+
+                    Response.Write(li.Text + "<br/>");
+                    Response.Write(li.Value + "<br/>");
+
+                
+                }
+
+             
+
+            
+            }
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+             foreach (ListItem li in CheckBoxList1.Items)
+                {
+
+                    li.Selected = true;
+                
+                
+                }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+             foreach (ListItem li in CheckBoxList1.Items)
+                {
+
+                    li.Selected = false;
+                
+                
+                }
+        }
+
+
+
+
+    }
+}
