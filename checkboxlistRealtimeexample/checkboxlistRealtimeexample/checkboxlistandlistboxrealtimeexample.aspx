6eb5d6603081d2002ebf5e2779f484a040b2ad3c
abc <you@example.com>
2018-02-20 13:16:56 +0600

    adding real time dropdown example

---
+++ checkboxlistRealtimeexample/checkboxlistRealtimeexample/checkboxlistandlistboxrealtimeexample.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="checkboxlistandlistboxrealtimeexample.aspx.cs" Inherits="checkboxlistRealtimeexample.checkboxlistandlistboxrealtimeexample" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 223px">
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:CheckBoxList ID="CheckBoxList1" runat="server" Height="98px" OnSelectedIndexChanged="CheckBoxList1_SelectedIndexChanged"  Width="148px"  RepeatDirection="Horizontal" AutoPostBack="True" >
+
+            <asp:ListItem Text="alamin Graduate" Value="1"> </asp:ListItem>
+             <asp:ListItem Text="aklima " Value="2"> </asp:ListItem>
+            <asp:ListItem Text="Jahangir " Value="3"> </asp:ListItem>
+             <asp:ListItem Text="Joba  " Value="4"> </asp:ListItem>
+            <asp:ListItem Text="aysha " Value="5"> </asp:ListItem>
+
+
+        </asp:CheckBoxList>
+
+    
+        <asp:ListBox ID="ListBox1" SelectionMode="Multiple" runat="server" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged" Height="86px" Width="200px"></asp:ListBox>
+
+    
+    </div>
+        <asp:Label ID="Label1"  Font-Bold="true" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ checkboxlistRealtimeexample/checkboxlistRealtimeexample/checkboxlistandlistboxrealtimeexample.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace checkboxlistRealtimeexample
+{
+    public partial class checkboxlistandlistboxrealtimeexample : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            ListBox1.Items.Clear();//it will help to remove the lastinserty+prevoius items insertion 
+
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+
+                {
+
+                    //ListBox1.Items.Add(li);//it wil sect all items with setction sign
+                    ListBox1.Items.Add(li.Text);//it willl print only the value
+                }
+            
+            }
+
+
+            if (CheckBoxList1.SelectedIndex == -1)
+            {
+
+                Label1.ForeColor = System.Drawing.Color.Red;
+
+            }
+
+            else
+            {
+
+                Label1.ForeColor = System.Drawing.Color.Black;
+
+            }
+
+            Label1.Text = ListBox1.Items.Count.ToString() + "items(s) selected";
+
+
+        }
+
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
