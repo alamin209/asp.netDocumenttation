26e079aa7247f2ad4487766f2b8c31b6b5805fa9
abc <you@example.com>
2018-02-21 15:22:08 +0600

    i change thename it is mainly radio burtton example

---
+++ Bulledasp.net/Bulledasp.net/RadioButton.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RadioButton.aspx.cs" Inherits="Bulledasp.net.RadioButton" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="height: 45px">
+    
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged" RepeatDirection="Horizontal">
+         <asp:ListItem  Text="Red" Value="1" ></asp:ListItem>
+        <asp:ListItem  Text="Green" Value="2" ></asp:ListItem>
+        <asp:ListItem  Text="Black" Value="3" ></asp:ListItem>
+        <asp:ListItem  Text="White" Value="4" ></asp:ListItem>
+        <asp:ListItem  Text="efef" Value="5" ></asp:ListItem>
+
+        </asp:RadioButtonList>
+
+    
+    </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Deselectall" />
+    </form>
+</body>
+</html>
---
+++ Bulledasp.net/Bulledasp.net/RadioButton.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Bulledasp.net
+{
+    public partial class RadioButton : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+  
+            if (RadioButtonList1.SelectedItem != null)
+            {
+                Response.Write("Text " + RadioButtonList1.SelectedItem.Text+"</br>");
+                Response.Write("Value" + RadioButtonList1.SelectedItem.Value+ "</br>" );
+                Response.Write("Text" + RadioButtonList1.SelectedIndex.ToString());
+            }
+
+            else
+            {
+                Response.Write("Please Selected first");
+            
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+
+            RadioButtonList1.SelectedIndex = -1;//best one 
+
+
+            //foreach (ListItem li in RadioButtonList1.Items)
+            //{
+
+            //    if (li.Selected)
+            //    {
+
+            //        li.Selected = false;
+                
+            //    }
+          
+            
+            //}
+
+        }
+    }
+}
