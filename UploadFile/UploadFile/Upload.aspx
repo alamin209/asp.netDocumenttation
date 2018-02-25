e8a827a5587c9fdf6f21049bd5ddf29eab5391d5
abc <you@example.com>
2018-02-25 12:14:35 +0600

    Upload done

---
+++ UploadFile/UploadFile/Upload.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="UploadFile.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Uploadfile" />
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" ForeColor="Black"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ UploadFile/UploadFile/Upload.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UploadFile
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                string fileextenion = System.IO.Path.GetExtension(FileUpload1.FileName);
+                if (fileextenion.ToLower() != ".doc" && fileextenion.ToLower() != ".docx")
+                { 
+
+
+                 Label1.Text = "Please select the file is not corrct formate ";
+                 Label1.ForeColor = System.Drawing.Color.Red;
+                
+                }
+
+
+
+                else
+
+                {
+                    
+                   int filsize=FileUpload1.PostedFile.ContentLength ;    //1024bite*1024bitt=1048576*2=2megabyte
+                    if(filsize>2097152)
+
+                 {  FileUpload1.SaveAs(Server.MapPath("~/Image/" + FileUpload1.FileName));
+                    Label1.Text = "File upload successful";
+                   Label1.ForeColor = System.Drawing.Color.Green;
+                 }
+
+                    else
+                    {
+                    Label1.Text = "File contain more than 2Mb ";
+                  
+                    }
+
+                }
+            }
+            else
+            {
+                Label1.Text = "Please select the file";
+                Label1.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
