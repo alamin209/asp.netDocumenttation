0785938eb758a208acc7af11febfe569e5d28f88
abc <you@example.com>
2018-03-12 20:13:38 +0600

    added extra functionalty to celender

---
+++ Celender/Celender/advance caleder .aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="advance caleder .aspx.cs" Inherits="Celender.advance_caleder" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Calendar ID="Calendar1" runat="server" BackColor="#FFFFCC" BorderColor="#FFCC66" BorderWidth="1px" Caption="Traing " DayNameFormat="Full" FirstDayOfWeek="Thursday" Font-Names="Verdana" Font-Size="8pt" ForeColor="#663399" Height="200px" NextMonthText="&gt;&gt;" NextPrevFormat="ShortMonth" OnDayRender="Calendar1_DayRender" OnSelectionChanged="Calendar1_SelectionChanged" SelectionMode="DayWeekMonth" SelectMonthText="Select month" SelectWeekText="Selected week" ShowGridLines="True" Width="220px" OnVisibleMonthChanged="Calendar1_VisibleMonthChanged">
+            <DayHeaderStyle BackColor="#FF0066" Font-Bold="True" ForeColor="#99FFCC" Height="1px" />
+            <NextPrevStyle Font-Size="9pt" ForeColor="#FFFFCC" />
+            <OtherMonthDayStyle ForeColor="#CC9966" />
+            <SelectedDayStyle BackColor="#CCCCFF" Font-Bold="True" />
+            <SelectorStyle BackColor="#FFCC66" />
+            <TitleStyle BackColor="#990000" Font-Bold="True" Font-Size="9pt" ForeColor="#FFFFCC" />
+            <TodayDayStyle BackColor="#FFCC66" ForeColor="White" />
+        </asp:Calendar>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Celender/Celender/advance caleder .aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Celender
+{
+    public partial class advance_caleder : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+            foreach(DateTime selectedDateTime in Calendar1.SelectedDates  )
+            {
+            Response.Write(Calendar1.SelectedDate.ToString("d")+"</br>");
+            
+            }
+
+            
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (!e.Day.IsOtherMonth && e.Day.Date.Day % 2 == 0)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Red;
+                e.Cell.ForeColor = System.Drawing.Color.White;
+                e.Cell.Font.Bold = true;
+                e.Cell.Text = "XXXX";
+                e.Cell.ToolTip = "This is booked for this time";
+
+
+            }
+            else
+            {
+                e.Cell.ToolTip = "Aviable";
+
+            }
+
+        }
+
+        protected void Calendar1_VisibleMonthChanged(object sender, MonthChangedEventArgs e)
+        {
+            string nexttmongth = getmongth(e.NewDate.Month);
+            string  prevoius = getmongth(e.PreviousDate.Month);
+
+            Response.Write("the prevous month " + prevoius + " Present  " + nexttmongth);
+
+        }
+
+
+         private  string getmongth(int mongthnumbe)
+        {
+
+            switch (mongthnumbe)
+            { 
+            
+                case 1:
+             return "Jan";
+             case 2:
+             return "Feb";
+             case 3:
+             return "March";
+             case 4:
+             return "April";
+              case 5:
+             return "may";
+              
+               case 6:
+             return "Jun";
+               case 7:
+             return "July";
+              case 8:
+             return "Aug";
+              case 9:
+             return "Sep";
+              case 10:
+             return "acto";
+              case 11:
+             return "July";
+              case 12:
+             return "July";
+             default:
+             return "Invalid Month";
+            }
+        
+        }
+
+    }
+}
