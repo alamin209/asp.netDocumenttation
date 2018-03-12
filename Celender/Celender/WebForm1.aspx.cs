8ed47308b4e9f2a7ad9213f9091790adfc0f4e53
abc <you@example.com>
2018-03-12 16:12:11 +0600

    ading basic calender

---
+++ Celender/Celender/WebForm1.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Celender
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Calendar1.Visible = false;
+
+            }
+
+
+
+        }
+
+       
+
+
+        protected void ImageButton1_Click1(object sender, ImageClickEventArgs e)
+        {
+            if (Calendar1.Visible)
+            {
+                Calendar1.Visible = false;
+            }
+            else
+            {
+
+                Calendar1.Visible = true;
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calendar1_SelectionChanged(object sender, EventArgs e)
+        {
+           
+            TextBox1.Text = Calendar1.SelectedDate.ToString("d");
+            Calendar1.Visible = false;
+
+        }
+
+        protected void Calendar1_DayRender(object sender, DayRenderEventArgs e)
+        {
+            if (e.Day.IsOtherMonth || e.Day.IsWeekend)
+            {
+                e.Day.IsSelectable = false;
+                e.Cell.BackColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
