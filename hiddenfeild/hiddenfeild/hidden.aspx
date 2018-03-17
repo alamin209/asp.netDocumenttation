7fe8545e4c43a6afb6e48b2a48ea3d8932c38830
abc <you@example.com>
2018-03-17 22:49:36 +0600

    Somtime the query work some time not

---
+++ hiddenfeild/hiddenfeild/hidden.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="hidden.aspx.cs" Inherits="hiddenfeild.hidden" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 29px;
+        }
+        .auto-style2 {
+            height: 30px;
+        }
+    </style>
+</head>
+<body style="height: 181px">
+    <form id="form1" runat="server">
+    <div style="font-family:Arial">
+
+        
+        <table >
+            <tr>
+                <td class="auto-style1">Name<asp:TextBox ID="textname" runat="server" ></asp:TextBox>
+                 <asp:HiddenField ID="HiddenFeildId"  runat="server" />
+                </td>
+               
+            </tr>
+            <tr>
+                <td>DEPERTMENRT <asp:TextBox ID="textdept" runat="server"></asp:TextBox>
+
+                </td>
+               
+           
+            </tr>
+            <tr>
+                <td>GENDER
+                     <asp:TextBox ID="textgender" runat="server"></asp:TextBox>
+                  </td>
+   
+            </tr>
+
+            <tr>
+            
+              
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Update" OnClick="Button1_Click1" />
+                    <asp:Button ID="Button2" runat="server" Text="Load" />
+                </td>
+                 <td>
+                     &nbsp;</td>
+            </tr>  
+
+        </table>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ hiddenfeild/hiddenfeild/hidden.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace hiddenfeild
+{
+    public partial class hidden : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+             if (!IsPostBack)
+            {
+                gatemploy();
+            
+            }
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void gatemploy()
+
+        {
+            string CS = ConfigurationManager.ConnectionStrings["Employ"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+
+                string sqlquery = "select Id,Name,Devepartment ,Gender from  Employ where id=2";
+                SqlCommand cmd = new SqlCommand(sqlquery, con);
+                con.Open();
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    while (rdr.Read())
+                    {
+                        textname.Text = rdr["Name"].ToString();
+                        textdept.Text = rdr["Devepartment"].ToString();
+                        textgender.Text = rdr["Gender"].ToString();
+                        HiddenFeildId.Value = rdr["Id"].ToString();
+
+
+                    }
+                
+                
+                }
+
+            
+
+            
+            }
+        
+        }
+
+
+
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            {
+
+                string CS = ConfigurationManager.ConnectionStrings["Employ"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(CS))
+                {
+
+                    string sqlquery = "Update Employ set Name=@Name,  Devepartment=@Devepartment,Gender=@Gender where Id=@Id ";
+                    SqlCommand cmd = new SqlCommand(sqlquery, con);
+                    cmd.Parameters.AddWithValue("@Name", textname.Text);
+                    cmd.Parameters.AddWithValue("@Devepartment", textdept.Text);
+                    cmd.Parameters.AddWithValue("@Gender", textgender.Text);
+                    cmd.Parameters.AddWithValue("@Id", HiddenFeildId.Value);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    textname.Text = "";
+                    textdept.Text = "";
+                    textgender.Text = "";
+
+                }
+
+
+            }
+        }
+        
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            //gatemploy();
+        }
+    }
+}
