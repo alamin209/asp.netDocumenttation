be7efaa423b14fe899cabe389cc1b1860b2e0c9d
abc <you@example.com>
2018-04-06 17:32:00 +0600

    adding fucionlity

---
+++ admin_panel/admin_panel/WebForm1.aspx
@@ -0,0 +1,110 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="admin_panel.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div>
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="true"   OnSelectedIndexChanged ="DropDownList2_SelectedIndexChanged"  >
+                <asp:ListItem Text="Select the  user" Value="-1"></asp:ListItem>
+                <asp:ListItem Text="Select the  Admin" Value="admin"></asp:ListItem>
+                <asp:ListItem Text="Select the  user" Value="nonadmin"></asp:ListItem>
+            </asp:DropDownList>
+            <asp:Panel ID="adminpanel" runat="server">
+        <table>
+           
+               <tr id="MyRow1" runat="server">
+           <td> <h3>You are loggin in as a admin </h3></td>
+    </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="admin" runat="server" Text="Admin"></asp:Label></td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="user" runat="server" Text="admin Login"></asp:Label>
+
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+
+            </tr>
+            <tr>
+                <td>
+                    <asp:Label ID="Label3" runat="server" Text="User Detail"></asp:Label></td>
+                <td>
+                    <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+                </td>
+
+            </tr>
+
+        </table>
+                </asp:Panel>
+            <asp:Panel ID="nonadminpanel" runat="server">
+    <table>
+       <%-- <tr>
+            <td colspan="2">
+                <h3>You are loggin in as a Mike </h3>
+            </td>
+        </tr>--%>
+        <tr>
+            <td colspan="2">
+                <asp:Label ID="Label1" runat="server" Text="nonAdmin"  font-size="XX-Large"></asp:Label>
+
+            </td>
+            <%--<td>
+                <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+            </td>--%>
+
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label4" runat="server" Text="admin Login"></asp:Label></td>
+            <td>
+                <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+            </td>
+
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="Label5" runat="server" Text="User Detail"></asp:Label></td>
+            <td>
+                <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+            </td>
+
+        </tr>
+          <tr>
+            <td>
+                <asp:Label ID="Label6" runat="server" Text="User Detail"></asp:Label></td>
+            <td>
+                <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+            </td>
+
+        </tr>
+          <tr>
+            <td>
+                <asp:Label ID="Label7" runat="server" Text="User Detail"></asp:Label></td>
+            <td>
+                <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+            </td>
+
+        </tr>
+
+
+    </table>
+                </asp:Panel>
+     </div>
+    </form>
+    
+</body>
+</html>
---
+++ admin_panel/admin_panel/WebForm1.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace admin_panel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                //now i will add best way
+
+
+                adminpanel.Visible = false;
+                nonadminpanel.Visible = false;
+
+                //////////////////// not a best way ////////////////
+                //hideadmin();
+                //hidenoneadmin ();
+
+            }
+
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (DropDownList2.SelectedValue=="-1")
+            {
+
+                //hideadmin();
+                //hidenoneadmin();
+                adminpanel.Visible = false;
+                nonadminpanel.Visible = false;
+
+
+                
+
+            }
+
+           else if (DropDownList2.SelectedValue == "admin")
+            {
+
+                //showadmin();
+                //hidenoneadmin();
+
+                //Label1.Visible = false;
+                //Label4.Visible = false;
+                //TextBox5.Visible = false;
+                //Label5.Visible = false;
+                //Label6.Visible = false;
+                //TextBox6.Visible = false;
+                //TextBox7.Visible = false;
+                //Label7.Visible = false;
+                //TextBox8.Visible = false;
+
+
+                adminpanel.Visible = true;
+                nonadminpanel.Visible = false;
+            
+            }
+            else if (DropDownList2.SelectedValue == "nonadmin")
+            {
+                //admin.Visible = false;
+                //TextBox1.Visible = false;
+                //user.Visible = false;
+                //TextBox2.Visible = false;
+                //Label3.Visible = false;
+                //MyRow1.Visible = false;
+
+                //TextBox3.Visible = false;
+
+                //shownonadmin();
+                //hideadmin();
+                adminpanel.Visible = false;
+                nonadminpanel.Visible = true;
+               
+
+
+            }
+
+           
+        }
+
+        private void showadmin()
+        {
+            admin.Visible = true;
+            TextBox1.Visible = true;
+            user.Visible = true;
+            TextBox2.Visible = true;
+            Label3.Visible = true;
+            MyRow1.Visible = true;
+            TextBox3.Visible = true;
+        }
+        private void shownonadmin()
+        {
+            Label1.Visible = true;
+            Label4.Visible = true;
+            TextBox5.Visible = true;
+            Label5.Visible = true;
+            Label6.Visible = true;
+            TextBox6.Visible = true;
+            TextBox7.Visible = true;
+            Label7.Visible = true;
+            TextBox8.Visible = true;
+        }
+        private void hideadmin()
+        {
+            admin.Visible = false;
+            TextBox1.Visible = false;
+            user.Visible = false;
+            TextBox2.Visible = false;
+            Label3.Visible = false;
+        }
+        private void hidenoneadmin()
+        {
+            TextBox3.Visible = false;
+            Label1.Visible = false;
+            Label4.Visible = false;
+            TextBox5.Visible = false;
+            Label5.Visible = false;
+            Label6.Visible = false;
+            TextBox6.Visible = false;
+            TextBox7.Visible = false;
+            Label7.Visible = false;
+            TextBox8.Visible = false;
+            MyRow1.Visible = false;
+
+        }
+
+
+
+
+    }
+}
